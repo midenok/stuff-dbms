3007fe97ee3e1c2168cb53e6b4857a4536adc28d
Aleksey Midenkov <midenok@gmail.com>
2026-06-16 13:31:58 +0300

    bush: bareize() v1: migrate/merge repo to centralized

--- bush/bin/env.sh
+++ bush/bin/env.sh
@@ -22,6 +22,7 @@ unset which
 export bush_dir=$(dirname $script)
 export src="${bush_dir}/src"
 export proj_dir=$(readlink -ne "${bush_dir}/..")
+export bare_dir="${proj_dir}/mariadb.git"
 export log_dir="${bush_dir}/log"
 export tarball_dir="/home/ec2-user"
 
@@ -47,7 +48,7 @@ opt_ddl="--mysqld=--debug=d,ddl_log"
 opt_vers="--mysqld=--debug=d,sysvers_force --mysqld=--system_versioning_alter_history=keep"
 opt_fts="--mysqld=--innodb_ft_sort_pll_degree=1"
 
-if [ -f $detection_file2 ] || (
+if [ -f $detection_file2 ] || [ ! -f $detection_file ] || (
     ! grep -iq mariadb $detection_file &&
     grep -iq mysql $detection_file)
 then
@@ -514,6 +515,7 @@ initdb()
     then
         cd "${bush_dir}"
         mkdir -p run
+        defaults=./mysqld.cnf
     fi
     data=$(readlink -f "${data}")
     if [ -e "${data}" ]
@@ -638,6 +640,13 @@ die()
         exit 1
     fi
 }
+
+die2()
+{
+    [ -n "$1" ] && echo "$1" >&2;
+    exit 1
+}
+export -f die2
 
 opt_matches()
 {
@@ -879,8 +888,10 @@ prepare()
     [ $product = mysql ] &&
         cclauncher="${cclauncher} -DWITH_SASL=no -DWITH_FIDO=none -DWITH_BUNDLED_LIBEVENT:BOOL=OFF -D WITH_BUNDLED_MEMCACHED:BOOL=OFF -DWITH_EMBEDDED_SERVER:BOOL=OFF -DWITH_EMBEDDED_SHARED_LIBRARY:BOOL=OFF -DWITH_HYPERGRAPH_OPTIMIZER:BOOL=OFF -DWITH_NDBAPI_EXAMPLES:BOOL=OFF -DWITH_NDBCLUSTER_STORAGE_ENGINE:BOOL=OFF -DWITH_NDBMTD:BOOL=OFF -DWITH_NDB_BINLOG:BOOL=OFF -DWITH_NDB_NODEJS:BOOL=OFF -DWITH_NDB_TEST:BOOL=OFF -DWITH_ROUTER:BOOL=OFF"
     # TODO: add DISABLE_PSI_FILE
-    eval flavor_opts=\$${flavor}_opts
+    flavor_safe=${flavor//[^a-zA-Z0-9_]/_}
+    eval flavor_opts=\$${flavor_safe}_opts
     get_linker_flags debug $debug_opts_linker
+
     cmake-ln -Wno-dev \
         -DCMAKE_INSTALL_PREFIX:STRING=${opt} \
         -DCMAKE_BUILD_TYPE:STRING=Debug \
@@ -908,7 +919,7 @@ prepare()
         -DWITH_MARIABACKUP:BOOL=OFF \
         -DWITH_READLINE:BOOL=ON \
         -DWITH_SAFEMALLOC:BOOL=OFF \
-        -DWITHOUT_ABI_CHECK:BOOL=OFF \
+        -DWITHOUT_ABI_CHECK:BOOL=ON \
         `# some older versions fail bootstrap on MD5 without SSL bundled` \
         -DWITH_SSL=$(for_mariadb bundled system) \
         "${cmake_flags[@]}" \
@@ -1671,6 +1682,251 @@ cut_thr()
     perl -pe 's/Thread \d+: //'
 }
 export -f cut_thr
+
+git_check_wt()
+{
+    git "$@" rev-parse --is-inside-work-tree > /dev/null 2>&1
+}
+export -f git_check_wt
+
+git_copy_remotes()
+{
+    local src_repo=$1
+    local dst_repo=$2
+    local copied=0
+    local skipped=0
+    local ignored=0
+
+    local -A url_to_name
+    local -A name_to_url
+
+    # 1. Read existing remotes in dst_repo"
+    # format of git remote -v: "name url (type)"
+    while read -r name url type
+    do
+        [[ "$type" != "(fetch)" ]] &&
+            continue
+        if [[ -z "${url_to_name["$url"]}" ]]
+        then
+            url_to_name["$url"]="$name"
+        else
+            url_to_name["$url"]+=" $name"
+        fi
+        name_to_url["$name"]="$url"
+    done < <(git -C "$dst_repo" remote -v)
+
+    # 2. Copy remotes from src_repo to dst_repo
+    while read -r name url type
+    do
+        [[ "$type" != "(fetch)" ]] &&
+            continue
+
+        local existing_name="${url_to_name["$url"]}"
+
+        # If url already exists in dst_repo skip adding the remote
+        if [[ -n "$existing_name" ]]
+        then
+            local last_name="${existing_names##* }"
+            local count=$(wc -w <<< "$existing_name")
+            ((skipped+=count)) || true
+        else
+            # Check if the name is already taken using the memory hash
+            local existing_url="${name_to_url["$name"]}"
+
+            if [[ -n "$existing_url" ]]; then
+                local err="Cannot add '$name' ($url), the name is already taken by a different url ($existing_url)!"
+                echo ":: Warning: ${err}" >&2
+                ((ignored++)) || true
+            else
+                echo ":: Adding remote '$name' ($url)"
+                git -C "$dst_repo" remote add "$name" "$url"
+
+                # Update both hashes to prevent internal duplicates within src_repo
+                url_to_name["$url"]="$name"
+                name_to_url["$name"]="$url"
+                ((copied++)) || true
+            fi
+        fi
+    done < <(git -C "$src_repo" remote -v)
+    echo ":: Remotes copied: ${copied}; skipped: ${skipped}; ignored: ${ignored}"
+}
+export -f git_copy_remotes
+
+git_list_branches()
+{
+    local repo=$1
+    shift
+    git -C "$repo" for-each-ref "$@" --format='%(refname:short)' refs/heads
+}
+export -f git_list_branches
+
+git_merge_repos()
+{
+    local src_repo=$1
+    local dst_repo=$2
+    local cur_src_branch=$3
+    local prefix=$(basename "$bush_dir")
+    local max_branches=10
+
+    # Out variable
+    new_branch=""
+
+    echo ":: Migrating last ${max_branches} local branches"
+
+    local src_branch
+
+    while read -r src_branch
+    do
+        [[ -z "$src_branch" ]] &&
+            continue
+
+        local dst_branch="$src_branch"
+
+        # If dst_branch already exists make it unique by prefixing
+        local i=0
+        while git -C "$dst_repo" show-ref --verify --quiet "refs/heads/$dst_branch"
+        do
+            dst_branch="${prefix}_${i}/${src_branch}"
+            ((i++)) || true
+        done
+
+        local cur=""
+        local suff=""
+        if [[ "$src_branch" == "$cur_src_branch" ]]
+        then
+            new_branch="$dst_branch"
+            cur="* "
+        fi
+
+        [[ "$src_branch" != "$dst_branch" ]] &&
+            suff="-> ${dst_branch}"
+
+        echo "${cur}${src_branch}${suff}"
+
+        # Now the main magic happens, import src_branch to dst_repo
+        git -C "$dst_repo" fetch "$src_repo" "+refs/heads/$src_branch:refs/heads/$dst_branch" --quiet
+
+        # Reattach the tracking data
+        local remote=$(git -C "$src_repo" config "branch.$src_branch.remote" || true)
+        local merge=$(git -C "$src_repo" config "branch.$src_branch.merge" || true)
+
+        if [[ -n "$remote" && -n "$merge" ]]
+        then
+            git -C "$dst_repo" config "branch.$dst_branch.remote" "$remote"
+            git -C "$dst_repo" config "branch.$dst_branch.merge" "$merge"
+        elif [[ -n "$remote" || -n "$merge" ]]
+        then
+            echo ":: Warning: source branch '$src_branch' broken tracking remote='$remote' merge='$merge'; skipped tracking restore"
+        fi
+        # FIXME: there can be no current branch in last branches vvv, add cur branch explicitly
+    done < <(git_list_branches "$src_repo" --sort=-committerdate --count=$max_branches)
+
+    [[ -z "$new_branch" ]] &&
+        die2 ":: Failed to map current src branch '$cur_src_branch' into dst_repo!"
+
+    true
+}
+export -f git_merge_repos
+
+### Conversion to v2, the below commands are adapted for help system of v2
+### The above commands work too, but without help
+### FIXME: make help system compatible with Doxygen
+
+## @brief Convert standalone bush to multi-worktree
+##
+## @details
+## Convert src/ to global $proj_dir/mariadb.git
+## Current branch and remote tracking (if any) is saved src/
+##
+## @note: v1 cannot work with existing $proj_dir/mariadb.git
+##
+## TODO: v2 should be able to merge remotes from src/ into $proj_dir/mariadb.git,
+##       excluding the duplicates. v3 should merge remote objects as well.
+##
+## Version: v1
+
+bareize()
+{(
+    # Avoid need git() wrapper
+    unset git
+    cd "$src"
+    git_check_wt ||
+        die2 "Not a git repository!"
+    git update-index -q --refresh
+    if [ -x "$bare_dir" ]
+    then
+        git_check_wt -C "$bare_dir" ||
+            die2 "Existing ${bare_dir} is not git repo!"
+        [ $(git -C "$bare_dir" config --get core.bare) != true ] &&
+            die2 "Existing repo ${bare_dir} is not bare!"
+    fi
+    old_git="${src}/.git"
+    [ -x "$old_git" ] ||
+        die2 "${old_git} doesn't exist!"
+    [[ $(git config --get core.bare) == "true" ]] &&
+        die2 "Old repo ${src} is bare!"
+    set -e
+    # How it works on detached HEAD? Maybe do git branch --show-current instead?
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    id_file=$(mktemp /tmp/bareize.XXXXXX)
+    id=$(basename "$id_file")
+    echo ":: Stashing your stuff by ${id}"
+    set > "$id_file"
+    git stash push --all -m "bareize stuff for ${id} (bush)"
+
+    if [ -x "$bare_dir" ]
+    then
+        echo ":: Attaching to bare repo at ${bare_dir}"
+        git_copy_remotes "$src" "$bare_dir"
+        git_merge_repos "$src" "$bare_dir" "$branch"
+
+        cd "$bare_dir"
+        # Get stashed files
+        # Note: we cannot just fetch into refs/stash, on restore it fails:
+        # git update-ref refs/tmp/local-stash refs/stash
+        # git update-ref -d refs/stash
+        # git fetch "$src" "refs/stash:refs/stash"
+        # git stash pop
+        # error: refs/stash@{0} is not a valid reference
+        # So, work this out via tmp branch:
+        git fetch "$src" "refs/stash:refs/tmp/${id}"
+
+        # FIXME: replace by rm
+        src_dir=$(basename "$src")
+        nogit=$(mktemp -up $bush_dir "${src_dir}_XXXXXX")
+        mv "$src" "$nogit"
+
+        git worktree add --force "$src" "$new_branch"
+
+        cd "$src"
+        echo ":: Restoring your stuff"
+        # Skip first two stash-internal commits: metadata, index; the third one is untracked files
+        git restore --recurse-submodules=no --overlay -s "refs/tmp/${id}^3" -W -- .
+        # git update-ref -d "refs/tmp/${id}"
+        # FIXME: submodule update --init? something broken with them?
+    else
+        echo ":: Creating new bare repo at ${bare_dir}"
+        mv "$old_git" "$bare_dir"
+        cd "$bare_dir"
+        git config core.bare true
+        # Old $src is not needed since the stuff is stashed
+        rm -rf "$src"
+
+        # Or instead of rm do this:
+        # src_dir=$(basename "$src")
+        # nogit=$(mktemp -up $bush_dir "${src_dir}.XXXXXX")
+        # mv "$src" "$nogit"
+
+        # --force proceeds on already checked out branches
+        git worktree add --force "$src" "$branch"
+        cd "$src"
+        echo ":: Restoring your stuff"
+        git stash pop --index
+    fi
+
+    rm "$id_file"
+)}
+export -f bareize
 
 [[ -f ~/work.sh ]] &&
   source ~/work.sh
